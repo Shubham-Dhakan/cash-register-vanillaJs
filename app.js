de59fe15473ced907a69b61ac0e2518d1ea5d794
Shubham Bhavin Dhakan <103954473+Shubham-Dhakan@users.noreply.github.com>
2023-01-27 22:29:51 -0500

    commit and push modified files to repo

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-
+const noOfNotes = document.querySelector(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
     if (billAmount.value > 0)
     {
         if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+            var amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         } else {
-            showMessage("The cahs provided should be atleast eqaul to bill amount. ");
+            showMessage("The cash provided should be atleast eqaul to bill amount. ");
         }
     } else {
         showMessage("Invalid Bill Amount");
@@ -21,10 +22,15 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 
 function calculateChange(amountToBeReturned) {
     for (let i=0;i<availableNotes.length;i++) {
-
+            const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
+            amountToBeReturned = amountToBeReturned %= availableNotes[i];
+            noOfNotes[i].innerText = numberOfNotes;
     }
 }
 
+function hideMessage(msg) {
+    message.style.display = "none";
+}
 function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
