6352563496e6f62ce610fa0790938542ea7aabaa
Shubham Bhavin Dhakan <103954473+Shubham-Dhakan@users.noreply.github.com>
2023-01-27 20:27:47 -0500

    Committing and Pushing modified files

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 const billAmount = document.querySelector("#bill-amount");
-const cahsGiven = document.querySelector("#cash-given");
+const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
 
-checkButton.addEventListener("click", () => console.log("clicked"));
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    if (billAmount.value > 0)
+    {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("The cahs provided should be atleast eqaul to bill amount. ");
+        }
+    } else {
+        showMessage("Invalid Bill Amount");
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+    for (let i=0;i<availableNotes.length;i++) {
+
+    }
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
