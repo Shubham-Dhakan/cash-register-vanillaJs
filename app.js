4cdee10b9ba524bf2465711eedc5aeea8bcc4c0b
Shubham Bhavin Dhakan <103954473+Shubham-Dhakan@users.noreply.github.com>
2023-01-29 07:48:05 -0500

    Updating and Committing new files to repo

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
     if (billAmount.value > 0)
     {
-        if (cashGiven.value >= billAmount.value) {
+        if (cashGiven.value > billAmount.value) {
             var amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         } else {
@@ -28,7 +28,7 @@ function calculateChange(amountToBeReturned) {
     }
 }
 
-function hideMessage(msg) {
+function hideMessage() {
     message.style.display = "none";
 }
 function showMessage(msg) {
